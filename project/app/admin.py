27e2de93b1dd2d85cff145e95962c0de92680d94
jongmee <ra123221@gmail.com>
2022-08-09 14:45:54 +0900

    model update & filter search test 2

--- project/app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Perfume
-
-# Register your models here.
-admin.site.register(Perfume)
-admin.site.register(Perfume)
-admin.site.register(Perfume)
-
-

30e30b92051d1ab4ea4484499dbfe734f474d3bc
brighteast98 <hdsung98@gmail.com>
2022-08-18 22:39:22 +0900

    survey_exceptSNS

--- survey/views.py
+++ survey/views.py
@@ -7,8 +7,18 @@ def home(request):
 def survey1(request):
     return render(request, 'survey1.html')
 
+def survey3(request):
+    return render(request, 'survey3.html')
+
 def form1(request):
     return render(request, 'form1.html')
 
+def form3(request):
+    return render(request, 'form3.html')
+
 def result1(request):
     return render(request, 'result1.html')
+
+def result3(request):
+    return render(request, 'result3.html')
+

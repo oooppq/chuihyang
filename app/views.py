41e4e9a3e1015d4fc33d4a32acf25caff372d5f6
jongmee <ra123221@gmail.com>
2022-08-19 20:07:45 +0900

    임시

--- app/views.py
+++ app/views.py
@@ -3,7 +3,8 @@ from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from .models import Perfume, PostModel, Review
 from .forms import PostForm, CommentForm
-from django.utils import timezone
+# from django.utils import timezone
+import random
 
 # library for query
 from django.db.models import Q
@@ -19,23 +20,37 @@ def perfumes(request, id):
     # review_form = ReviewForm()
     perfume = get_object_or_404(Perfume, id=id)
     reviews = Review.objects.filter(perfume=perfume).order_by('-date')
-    return render(request, 'perfume.html', {"perfume": perfume, 'reviews':reviews})
+    flavor1 = perfume.flavor
+    print(flavor1)
+    rec = Perfume.objects.filter(flavor=flavor1)[1:6]
+    print(rec)
+    return render(request, 'perfume.html', {"perfume": perfume, 'reviews': reviews, 'rec':rec})
 
 
 def new_review(request, id):
     new = Review()
     new.content = request.POST['content']
     new.perfume = get_object_or_404(Perfume, pk=id)
-    try: new.user = request.user
-    except: pass
-    try: new.rating = request.POST['reviewStar']
-    except: pass
-    try: new.first_scent = request.POST['first_scent']
-    except: pass
-    try: new.put_scent = request.POST['put_scent']
-    except: pass
-    try: new.rest_scent = request.POST['rest_scent']
-    except: pass
+    try:
+        new.user = request.user
+    except:
+        pass
+    try:
+        new.rating = request.POST['reviewStar']
+    except:
+        pass
+    try:
+        new.first_scent = request.POST['first_scent']
+    except:
+        pass
+    try:
+        new.put_scent = request.POST['put_scent']
+    except:
+        pass
+    try:
+        new.rest_scent = request.POST['rest_scent']
+    except:
+        pass
 
     # 다중 선택 항목
     type_list = request.POST.getlist('type_list')
@@ -89,7 +104,6 @@ def category_detail(request, id):
     elif id == 9:
         pass
 
-
     return render(request, 'category_detail.html')
 
 
@@ -100,10 +114,10 @@ def survey(request):
 #### 게시판 페이지 ####
 def board(request):
     posts = PostModel.objects.filter().order_by('-date')
-    paginator=Paginator(posts, 5)
-    pagnum=request.GET.get('page')
+    paginator = Paginator(posts, 5)
+    pagnum = request.GET.get('page')
     posts = paginator.get_page(pagnum)
-    return render(request, 'board.html', {'posts':posts})
+    return render(request, 'board.html', {'posts': posts})
 
 
 #### 게시물 작성 ####
@@ -115,7 +129,7 @@ def postcreate(request):
             return redirect('board')
     else:
         form = PostForm()
-    return render(request, 'postcreate.html', {'form':form})
+    return render(request, 'postcreate.html', {'form': form})
 
 
 #### 개별 게시물 보여주기 ####
@@ -125,6 +139,8 @@ def post_detail(request, post_id):
     return render(request, 'post-detail.html', {'post': post, 'comment_form': comment_form})
 
 #### 게시물 댓글 작성 ####
+
+
 def new_comment(request, post_id):
     filled_form = CommentForm(request.POST)
     if filled_form.is_valid():
@@ -134,24 +150,31 @@ def new_comment(request, post_id):
     return redirect('post_detail', post_id)
 
 #### 게시물 삭제 ####
+
+
 def delete(request, post_id):
     object = get_object_or_404(PostModel, pk=post_id)
     object.delete()
     return redirect('board')
 
 #### 게시물 수정 ####
+
+
 def edit(request, post_id):
     post = PostModel.objects.get(id=post_id)    # 수정하고자 하는 객체 갖고 와서
     if request.method == "POST":            # 만일 request method가 POST라면
         form = PostForm(request.POST, request.FILES)    # 입력 내용을 갖고와서
         if form.is_valid():                             # 입력 내용 검수한 뒤
-            post.title = form.cleaned_data['title']     # 입력 내용 중 title을 수정하고자 하는 객체의 title에 저장!
-            post.body = form.cleaned_data['body']       # 입력 내용 중 body를 수정하고자 하는 객체의 body에 저장!
+            # 입력 내용 중 title을 수정하고자 하는 객체의 title에 저장!
+            post.title = form.cleaned_data['title']
+            # 입력 내용 중 body를 수정하고자 하는 객체의 body에 저장!
+            post.body = form.cleaned_data['body']
             post.save()                                 # 그리고 수정된 값을 저장한 객체는 저장
-            return redirect('/post_detail/'+str(post.pk))      # 수정이 되었으면 detail 페이지(해당 그 게시물 페이지)로 이동
+            # 수정이 되었으면 detail 페이지(해당 그 게시물 페이지)로 이동
+            return redirect('/post_detail/'+str(post.pk))
     else:                                        # 만일 request method가 GET이면
-        form = PostForm()                  
-        return render(request, 'post-edit.html',{'form':form})  # 입력 공간을 갖다준다
+        form = PostForm()
+        return render(request, 'post-edit.html', {'form': form})  # 입력 공간을 갖다준다
 
 
 def ranking(request):
@@ -175,7 +198,7 @@ def searched(request):
     flavor_list = request.GET.getlist('flavor_list')  # ['woody']
     gender_list = request.GET.getlist('gender_list')  # ['man']
     # perfumes = Perfume.objects.filter(name__contains=searched)
-    
+
     query = Q()
     for i, Season in enumerate(season_list):
         if i == 0:
@@ -200,14 +223,17 @@ def searched(request):
         query = query & Q(name__contains=searched)
     print(query)
     width = int(request.GET['window-width'])
-    if width > 820: numToDisplay = 14
-    elif width > 630: numToDisplay = 6
-    else: numToDisplay = 4
+    if width > 820:
+        numToDisplay = 14
+    elif width > 630:
+        numToDisplay = 6
+    else:
+        numToDisplay = 4
     perfumes = Perfume.objects.filter(query)
     print(len(perfumes))
     paginator = Paginator(perfumes, numToDisplay)
     print(paginator.num_pages)
-    pageNum = 1#request.GET.get('page')
+    pageNum = 1  # request.GET.get('page')
 
     perfumes = paginator.get_page(pageNum)
     return render(request, 'search-result.html', {'searched': searched, 'perfumes': perfumes})
